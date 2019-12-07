b0c65f2856ed4505892077a5a702de8a74fe5498
Q_uan <quan_qixian@163.com>
2019-12-07 11:30:30 +0800

    33_c++中的字符串类 2_字符串流类

---
+++ 33_c++中的字符串类/33_2字符串流类.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+#define TO_NUMBER(s, n) ( istringstream(s)>> n)
+#define TO_STRING(n)  (  ( (ostringstream&)(ostringstream() <<n) ).str()  )
+
+int main(int argc, const char *argv[])
+{
+	double n = 0;
+	if(TO_NUMBER("234.567",n))
+	{
+		cout<< n<<endl;
+	}
+	string s = TO_STRING(12345);
+	cout<< s<<endl;
+	return 0;
+}
