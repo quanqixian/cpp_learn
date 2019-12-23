32204456c94b1e7f5e4a2b0d696ddaaa32288a20
Q_uan <quan_qixian@163.com>
2019-12-23 23:36:09 +0800

    64_c++中的异常处理上

---
+++ 64_c++中的异常处理上/1_简单示例.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+double divide(double a, double b)
+{
+	const double delta=0.000000000000001;
+	double ret = 0; 
+
+	if( !((-delta<b)&&(b< delta ) ) )
+	{
+		ret= a/b;
+	}
+	else
+	{
+		throw 0;
+	}
+}
+int main(int argc, const char *argv[])
+{
+	try
+	{
+		double r= divide(1,0);
+		cout <<"r="<<r<<endl;
+	}
+	catch(...)
+	{
+		cout<<"Divided by zero..."<<endl;
+	}
+	return 0;
+}
