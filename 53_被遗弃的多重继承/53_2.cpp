4a3c9e47e676bd0160f0022b37a2f4b430ca2233
Q_uan <quan_qixian@163.com>
2020-05-19 21:25:10 +0800

    菱形继承数据冗余测试

--- 53_被遗弃的多重继承/53_2.cpp
+++ 53_被遗弃的多重继承/53_2.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class People
 {
-	string m_name;
-	int m_age;
+    string m_name;
+    int m_age;
 public:
-	People(string name, int age)
-	{
-		m_name = name;
-		m_age = age;
-	}
-	void print()
-	{
-		cout<<"Name = "<<m_name<<","
-			<<"Age ="<<m_age<<endl;
-	}
+    People(string name , int age)
+    {
+        m_name = name;
+        m_age = age;
+    }
+    void print()
+    {
+        cout<<"Name = "<<m_name<<", "
+            <<"Age = "<<m_age<<endl;
+    }
 };
-class Teacher:virtual public People
+
+class Teacher : virtual public People
 {
 public:
-	Teacher(string name ,int age ): People(name ,age)
-	{
-	}
+    Teacher(string name, int age) : People(name, age)
+    {
+    }
 };
-class Student:virtual public People 
+
+class Student : virtual public People
 {
 public:
-	Student(string name,int age):People(name,age)
-	{
-	}
+    Student(string name, int age) : People(name, age)
+    {
+    }
 };
-class Doctor:public Teacher,public Student 
+
+class Doctor : public Teacher, public Student
 {
 public:
-	Doctor(string name,int age):Teacher(name,age),Student(name,age),People(name,age)
-	{
-	}
+    Doctor(string name, int age) : Teacher(name, age), Student(name, age), People(name, age)
+    {
+    }
 };
+
 int main(int argc, const char *argv[])
 {
-	Doctor d("Delphi",333);
-	d.print();
-	return 0;
+    Doctor d("Quan", 27);
+    cout<<"sizeof(string) = "<<sizeof(string)<<endl;
+    cout<<"sizeof(People) = "<<sizeof(People)<<std::endl;
+    cout<<"sizeof(Teacher) = "<<sizeof(Teacher)<<std::endl;
+    cout<<"sizeof(Student) = "<<sizeof(Student)<<std::endl;
+    cout<<"sizeof(Doctor) = "<<sizeof(d)<<std::endl;
+    d.print();
+    return 0;
 }
-
